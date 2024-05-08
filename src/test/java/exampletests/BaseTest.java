8d0d5502f5a1117482907c42e915a842811c7d16
savcho1 <savchenko@gmail.com>
2024-05-08 09:02:15 +0200

    second commit

--- src/test/java/exampletests/BaseTest.java
+++ src/test/java/exampletests/BaseTest.java
@@ -9,11 +9,13 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.reporters.jq.Main;
 import pageobjects.BasePage;
 import pageobjects.Page;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class BaseTest {
@@ -27,21 +29,22 @@ public class BaseTest {
     public void setUpBrowser(String browser){
         if(browser.equals("chrome")){
             ChromeOptions options = new ChromeOptions();
-            options.setBinary("/usr/local/bin/chromedriver");
+          //  options.setBinary("/usr/local/bin/chromedriver");
+            options.setBinary("/src/maim/resources/chromedriver");
             driver.set(new ChromeDriver(options));
-
-            //WebDriverManager.chromedriver().setup();
-            //driver.set(new ChromeDriver());
+//            WebDriverManager.chromedriver().setup();
+//            driver.set(new ChromeDriver());
         }
         else if(browser.equals("ff")){
 //            WebDriverManager.firefoxdriver().setup();
 //            driver.set(new FirefoxDriver());
             FirefoxOptions options = new FirefoxOptions();
-            options.setBinary("/usr/local/bin/geckodriver");
+            //options.setBinary("/usr/local/bin/geckodriver");
+            options.setBinary("/src/maim/resources/geckodriver");
             driver.set(new FirefoxDriver(options));
         }
         else {
-            System.out.println("No browsers set up");
+            System.out.println("No browsers to set up");
         }
 
         driver.get().navigate().to(properties.getProperty("baseUrl"));
@@ -58,9 +61,11 @@ public class BaseTest {
     public static Properties readPropertyFile(){
         Properties properties = new Properties();
         try {
-            FileInputStream fileInputStream = new FileInputStream("/Users/yuliyafedko/Documents/Savchenko/testProject/src/main/resources/config.properties");
-            properties.load(fileInputStream);
-            fileInputStream.close();
+            ClassLoader classLoader = Main.class.getClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream("config.properties");
+
+            // Load properties from the input stream
+            properties.load(inputStream);
 
             // Get the value of the base URL property
         } catch (IOException e) {
